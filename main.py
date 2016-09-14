b732559534dc549dbf25dd7cf73f6b2e76cfe59a
AndrewScalise <paradigmcomplex@gmail.com>
2016-09-14 10:49:08 -0500

    Working blog

--- main.py
+++ main.py
@@ -1,25 +1,77 @@
-#!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 import webapp2
+import jinja2
+import os
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+# set up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class Blog(db.Model):
+    title = db.StringProperty(required = True)
+    thoughts = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+
     def get(self):
-        self.response.write('Hello world!')
+        blog = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
+        t = jinja_env.get_template("post-listings.html")
+        response = t.render(blogs = blog)
+
+        self.response.write(response)
+
+
+
+class PostForm(Handler):
+    def render_post_form(self, title="", thoughts="", error=""):
+        self.render("post-form.html", title=title, thoughts=thoughts, error=error)
+
+    def get(self):
+        self.render_post_form()
+
+    def post(self):
+        title = self.request.get("title")
+        thoughts = self.request.get("thoughts")
+
+        if title and thoughts:
+            a = Blog(title = title, thoughts = thoughts)
+            a.put()
+
+            self.redirect("/blog")
+
+        else:
+            error = "You need both a title and thoughts for a blog!"
+            self.render_post_form(title, thoughts, error)
+
+class ViewPostHandler(webapp2.RequestHandler):
+    def get(self, id):
+        post = Blog.get_by_id(int(id))
+
+        if post:
+            response = "<h1>" + post.title + "</h1>"+ "<div>" + post.thoughts + "</div>"
+            self.response.write(response)
+        else:
+            error="<h1>This id does not exist!</h1>"
+            self.response.write(error)
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+
+        ('/blog', MainPage),
+        ('/newpost', PostForm),
+        webapp2.Route('/blog/<id:\d+>', ViewPostHandler)
+
 ], debug=True)
